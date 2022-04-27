fb37ef1290b4c2f6614255011d1637a5211f1d38
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2022-04-27 11:43:45 -0700

    initial commit

---
+++ Swift Recipe App/Shared/Swift_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Recipe_AppApp.swift
+//  Shared
+//
+//  Created by Quinton Price on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
