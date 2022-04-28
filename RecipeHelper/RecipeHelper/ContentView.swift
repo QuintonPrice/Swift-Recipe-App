59c6a66ac95c2da97993af057734a425fc9978fb
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2022-04-27 17:31:46 -0700

    Beginner concepts

--- RecipeHelper/RecipeHelper/ContentView.swift
+++ RecipeHelper/RecipeHelper/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0;
+    @State private var name = "";
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Text("This is a new section")
+                    Text("This is part of the new section")
+                    Button("Tap Count: \(tapCount)") {
+                        tapCount += 1
+                    }
+                    Button("Reset Count") {
+                        tapCount = 0
+                    }
+                }
+                Section {
+                    TextField("Enter your recipe name: ", text: $name)
+                    Text("Your recipe's name is: \(name)")
+                }
+            }
+            .navigationTitle("Recipe Helper")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
