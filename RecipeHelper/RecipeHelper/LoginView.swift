db5df40cf319a28d5801752d3732b96bc5dc64ea
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2022-04-29 11:07:45 -0700

    Added login view

---
+++ RecipeHelper/RecipeHelper/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  RecipeHelper
+//
+//  Created by Quinton Price on 4/29/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login View")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
